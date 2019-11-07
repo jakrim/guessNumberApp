6d042fe7d3bb508c27d23bd6925168755958d238
Jesse Krim <jesse.krim@gmail.com>
2019-11-07 11:22:18 -0500

    Dynamicly style GameOverScreen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet, Image } from 'react-native';
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  Image,
+  Dimensions,
+  ScrollView
+} from 'react-native';
 import BodyText from '../components/BodyText';
 import TitleText from '../components/TitleText';
 import Colors from '../constants/colors';
@@ -7,26 +15,28 @@ import MainButton from '../components/MainButton';
 
 const GameOverScreen = props => {
   return (
-    <View style={styles.screen}>
-      <TitleText>The Game is Over!</TitleText>
-      <View style={styles.imageContainer}>
-        <Image
-          fadeDuration={1000}
-          source={require('../assets/success.png')}
-          style={styles.image}
-          resizeMode='cover' // DEFAULT VALUE
-        />
+    <ScrollView>
+      <View style={styles.screen}>
+        <TitleText>The Game is Over!</TitleText>
+        <View style={styles.imageContainer}>
+          <Image
+            fadeDuration={1000}
+            source={require('../assets/success.png')}
+            style={styles.image}
+            resizeMode='cover' // DEFAULT VALUE
+          />
+        </View>
+        <View style={styles.resultContainer}>
+          <BodyText style={styles.resultText}>
+            Your phone needed
+            <Text style={styles.highlight}> {props.roundsNumber}</Text> rounds
+            to guess the number
+            <Text style={styles.highlight}> {props.userNumber}</Text>
+          </BodyText>
+        </View>
+        <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
       </View>
-      <View style={styles.resultContainer}>
-        <BodyText style={styles.resultText}>
-          Your phone needed
-          <Text style={styles.highlight}> {props.roundsNumber}</Text> rounds to
-          guess the number
-          <Text style={styles.highlight}> {props.userNumber}</Text>
-        </BodyText>
-      </View>
-      <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
-    </View>
+    </ScrollView>
   );
 };
 
@@ -37,13 +47,13 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   imageContainer: {
-    width: 300,
-    height: 300,
-    borderRadius: 150,
+    width: Dimensions.get('window').width * 0.7,
+    height: Dimensions.get('window').width * 0.7,
+    borderRadius: (Dimensions.get('window').width * 0.7) / 2,
     borderWidth: 2,
     borderColor: 'black',
     overflow: 'hidden',
-    marginVertical: 30
+    marginVertical: Dimensions.get('window').height / 30
   },
   image: {
     width: '100%',
@@ -51,7 +61,7 @@ const styles = StyleSheet.create({
   },
   resultContainer: {
     marginHorizontal: 30,
-    marginVertical: 15
+    marginVertical: Dimensions.get('window').width / 60
   },
   highlight: {
     color: Colors.primary,
@@ -59,7 +69,7 @@ const styles = StyleSheet.create({
   },
   resultText: {
     textAlign: 'center',
-    fontSize: 20
+    fontSize: Dimensions.get('window').height < 400 ? 16 : 20
   }
 });
 
