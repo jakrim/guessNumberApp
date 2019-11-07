cf8ee72c9cc30317a7ad3e50562c100a616a980a
Jesse Krim <jesse.krim@gmail.com>
2019-11-07 11:16:41 -0500

    Add Dynamic sizing for different devices

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,12 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { View, Text, StyleSheet, Alert, ScrollView } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Alert,
+  ScrollView,
+  Dimensions
+} from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 import NumberContainer from '../components/NumberContainer';
@@ -65,6 +72,12 @@ const GameScreen = props => {
     setPastGuesses(currentPastGuesses => [nextNumber, ...currentPastGuesses]);
   };
 
+  let listContainerStyle = styles.listContainer;
+
+  if (Dimensions.get('window').width < 350) {
+    listContainerStyle = styles.listContainerBig;
+  }
+
   return (
     <View style={styles.screen}>
       <Text style={DefaultStyles.title}>Opponent's Guess</Text>
@@ -77,7 +90,7 @@ const GameScreen = props => {
           <Ionicons name='md-add' size={24} color='white' />
         </MainButton>
       </Card>
-      <View style={styles.listContainer}>
+      <View style={listContainerStyle}>
         <ScrollView contentContainerStyle={styles.list}>
           {pastGuesses.map((guess, index) =>
             renderListItem(guess, pastGuesses.length - index)
@@ -97,11 +110,16 @@ const styles = StyleSheet.create({
   buttonContainer: {
     flexDirection: 'row',
     justifyContent: 'space-around',
-    marginTop: 20,
+    marginTop: Dimensions.get('window').height > 600 ? 20 : 5,
     width: 400,
     maxWidth: '90%'
   },
   listContainer: {
+    flex: 1,
+    width: '60%',
+    padding: 10
+  },
+  listContainerBig: {
     flex: 1,
     width: '80%',
     padding: 10
@@ -119,7 +137,7 @@ const styles = StyleSheet.create({
     padding: 15,
     marginVertical: 10,
     backgroundColor: 'white',
-    width: '60%'
+    width: '100%'
   }
 });
 
